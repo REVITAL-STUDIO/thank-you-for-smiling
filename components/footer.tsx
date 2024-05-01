2e76dab508585564d4bd6b4affe0d5b34b3701e2
roycejwilliams <roycewilliamsj@gmail.com>
2024-05-01 13:15:50 -0500

    update homepage

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,48 +4,39 @@ import { FaFacebook, FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 const nav = () => {
+  return (
+    <div className="flex justify-between w-full bg-[#171738] items-center h-300">
+      <div className="flex flex-col gap-4 w-1/4 h-full justify-center p-4">
+        <Image src={"/images/logo.png"} height={200} width={200} alt="logo" />
+        <p className="text-[#F1C900] text-xs">
+          Thank You for Smiling. All rights reserved.
+        </p>
+        <p className="text-white  text-xs">
+          Developed by <span className="font-bold">REVITAL Studio</span>
+        </p>
+      </div>
 
-
-    return (
-        <nav className="w-full">
-        <div className="flex justify-between bg-[#171738] items-end px-[3rem] py-[1.5rem]">
-          <div className="flex flex-col gap-6">
-            <Image
-              src={"/images/logo.png"}
-              height={300}
-              width={300}
-              alt="logo"
-            />
-            <p className="text-[#F1C900]">Thank You for Smilining. All rights reserved.</p>
-          </div>
-          <p className="text-white relative ">Developed by <span className="font-bold">REVITAL Studio</span></p>
-          <div className="flex flex-col text-white min-w-[24rem] text-lg gap-4">
-            <div className="flex items-center justify-between border-b-[2px] border-white w-full py-2">
-              <a>About Us</a>
-              <MdOutlineArrowRightAlt className="h-[2rem] w-[2rem]" />
-            </div>
-            <div className="flex items-center justify-between border-b-[2px] border-white min-w-full py-2">
-              <a>Programs</a>
-              <MdOutlineArrowRightAlt className="h-[2rem] w-[2rem]" />
-
-            </div>
-            <div className="flex items-center justify-between border-b-[2px] border-white min-w-full py-2">
-              <a>Donate</a>
-              <MdOutlineArrowRightAlt className="h-[2rem] w-[2rem]" />
-            </div>
-            <div className="flex items-center justify-between border-b-[2px] border-white min-w-full py-2">
-              <a>Book</a>
-              <MdOutlineArrowRightAlt className="h-[2rem] w-[2rem]" />
-            </div>
-            <div className="flex gap-8 justify-end">
-              <FaFacebook className="h-[2rem] w-[2rem]"/>
-              <FaInstagram className="h-[2rem] w-[2rem]"/>
-              <FaXTwitter className="h-[2rem] w-[2rem]"/>
-            </div>
-          </div>
+      <div className="flex flex-col text-white w-1/5  text-base  px-4">
+        <div className="flex items-center   border-white min-w-1/2 py-2 ">
+          <a>About Us</a>
         </div>
-      </nav>
-    )
-}
+        <div className="flex items-center   border-white min-w-1/2 py-2 ">
+          <a>Programs</a>
+        </div>
+        <div className="flex items-center   border-white min-w-1/2 py-2 ">
+          <a>Donate</a>
+        </div>
+        <div className="flex items-center   border-white min-w-1/2 py-2 ">
+          <a>Book</a>
+        </div>
+        <div className="flex gap-8 ">
+          <FaFacebook className="h-6 w-6" />
+          <FaInstagram className="h-6 w-6" />
+          <FaXTwitter className="h-6 w-6" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default nav;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        kanit: ['Kanit', 'sans-serif'],
+        dmSans: ['DM Sans', 'sans-serif']
+      },  
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      height: {
+        80: "80px",
+        100: "100px",
+        200: "200px",
+        300: "300px",
+        500: "500px",
+        600: "600px",
+        750: "740px"
+
+      }
     },
   },
   plugins: [],
