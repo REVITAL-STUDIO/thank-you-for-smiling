6caf221039f8099818ccc0987761a7ec308caa05
roycejwilliams <roycewilliamsj@gmail.com>
2024-05-02 11:57:12 -0500

    Still working on homepage just moved on to the next page for now

--- app/program/page.tsx
+++ app/program/page.tsx
@@ -2,11 +2,7 @@ import Nav from "@/components/nav";
 import React from "react";
 
 function Programs() {
-  return (
-    <div>
-      <Nav />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Programs;
